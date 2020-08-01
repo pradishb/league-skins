460f55bf5a041909ddad1e7413c16effe61e4a99
Rajesh Ligal <rajeshligal21@gmail.com>
2020-08-01 17:46:24 +0545

    Updates

---
+++ precache-manifest.b659141749c85ce9d187eeb7df65a7dc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d6d04acb961fa9bbc03326f8373cb55",
+    "url": "/league-skins/index.html"
+  },
+  {
+    "revision": "26bff38efd3d8aa7aa5b",
+    "url": "/league-skins/static/css/main.5dbb4e95.chunk.css"
+  },
+  {
+    "revision": "d8d95b40ed8e550e8091",
+    "url": "/league-skins/static/js/2.11d8f435.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/league-skins/static/js/2.11d8f435.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26bff38efd3d8aa7aa5b",
+    "url": "/league-skins/static/js/main.f32799ee.chunk.js"
+  },
+  {
+    "revision": "f6d424476f3437247d28",
+    "url": "/league-skins/static/js/runtime-main.9fc88f0b.js"
+  },
+  {
+    "revision": "60fe8d7426603b2de93c7da56f68a92c",
+    "url": "/league-skins/static/media/img.60fe8d74.jpg"
+  },
+  {
+    "revision": "18a1c553c84ceee993712959b62bfc30",
+    "url": "/league-skins/static/media/profile.18a1c553.jpg"
+  }
+]);
