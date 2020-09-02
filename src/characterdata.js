3224a76f19c375ce9e761ce08d961a2f89e2645f
Rajesh Ligal <rajeshligal21@gmail.com>
2020-09-03 02:31:14 +0545

    Adds Card hover Transitions ease out and character data

--- src/characterdata.js
+++ src/characterdata.js
@@ -32,46 +32,46 @@ const characterdata = [
       "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt40dfbe48a61c439f/5db05fa80b39e86c2f83dbf9/RiotX_ChampionList_amumu.jpg?quality=90&width=250",
   },
   {
-    id: 5,
-    name: "AMUNU",
+    id: 6,
+    name: "ANIVA",
     img:
-      "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt40dfbe48a61c439f/5db05fa80b39e86c2f83dbf9/RiotX_ChampionList_amumu.jpg?quality=90&width=250",
+      "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt3d24a1482453088a/5db05fa8df78486c826dcce8/RiotX_ChampionList_anivia.jpg?quality=90&width=250",
   },
   {
-    id: 5,
-    name: "AMUNU",
+    id: 7,
+    name: "ANNIE",
     img:
-      "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt40dfbe48a61c439f/5db05fa80b39e86c2f83dbf9/RiotX_ChampionList_amumu.jpg?quality=90&width=250",
+      "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt28c708665427aef6/5db05fa89481396d6bdd01a6/RiotX_ChampionList_annie.jpg?quality=90&width=250",
   },
   {
-    id: 5,
-    name: "AMUNU",
+    id: 8,
+    name: "APHELIOS",
     img:
-      "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt40dfbe48a61c439f/5db05fa80b39e86c2f83dbf9/RiotX_ChampionList_amumu.jpg?quality=90&width=250",
+      "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aphelios_0.jpg",
   },
   {
-    id: 5,
-    name: "AMUNU",
+    id: 9,
+    name: "ASHE",
     img:
-      "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt40dfbe48a61c439f/5db05fa80b39e86c2f83dbf9/RiotX_ChampionList_amumu.jpg?quality=90&width=250",
+      "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt943aae038e2dee98/5db05fa8e9effa6ba5295f91/RiotX_ChampionList_ashe.jpg?quality=90&width=250",
   },
   {
-    id: 5,
-    name: "AMUNU",
+    id: 10,
+    name: "AURELION SOL",
     img:
-      "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt40dfbe48a61c439f/5db05fa80b39e86c2f83dbf9/RiotX_ChampionList_amumu.jpg?quality=90&width=250",
+      "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt5dd3569fc67d6664/5db05fa8bd24496c390ae4d8/RiotX_ChampionList_aurelionsol.jpg?quality=90&width=250",
   },
   {
-    id: 3,
-    name: "AKALI",
+    id: 11,
+    name: "AZIR",
     img:
-      "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt80ff58fe809777ff/5db05fa8adc8656c7d24e7d6/RiotX_ChampionList_akali.jpg?quality=90&width=250",
+      "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt0e3f847946232167/5db05fa889fb926b491ed7ff/RiotX_ChampionList_azir.jpg?quality=90&width=250",
   },
   {
-    id: 3,
-    name: "AKALI",
+    id: 12,
+    name: "BARD",
     img:
-      "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt80ff58fe809777ff/5db05fa8adc8656c7d24e7d6/RiotX_ChampionList_akali.jpg?quality=90&width=250",
+      "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/bltbbe3ce0c0ae1305b/5db05fb23a326d6df6c0e7b3/RiotX_ChampionList_bard.jpg?quality=90&width=250",
   },
 ];
 
