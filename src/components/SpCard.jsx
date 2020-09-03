785d43a3d42c22459b785ecdb75b60aab7dbd1e4
Rajesh Ligal <rajeshligal21@gmail.com>
2020-09-04 03:31:08 +0545

    Adds Functional Star Rating

--- src/components/SpCard.jsx
+++ src/components/SpCard.jsx
@@ -1,19 +1,24 @@
-import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
+import ReactStars from "react-rating-stars-component";
 
 function SpCard(props) {
+  const ratingChanged = (newRating) => {
+    console.log(newRating);
+  };
+
   return (
     <div className="sp-card">
       <img src={props.img} alt="image" />
       <div className="sp-card-rating">
         <div className="sdf-h5 dark">{props.name}</div>
-        <div className="rating">
-          <FontAwesomeIcon icon="star" color="white" />
-          <FontAwesomeIcon icon="star" color="white" />
-          <FontAwesomeIcon icon="star" color="white" />
-          <FontAwesomeIcon icon="star" color="white" />
-          <FontAwesomeIcon icon="star-half" color="white" />
-        </div>
+
+        <ReactStars
+          count={5}
+          onChange={ratingChanged}
+          size={24}
+          activeColor="#ffd700"
+          classNames="rating-star"
+        />
       </div>
     </div>
   );
